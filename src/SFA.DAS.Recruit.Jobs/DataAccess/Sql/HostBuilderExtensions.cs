4a19c527cc5b6f5639619b48337f913df5c12252
killij <51908793+killij@users.noreply.github.com>
2025-04-03 09:13:05 +0100

    FAI-2394 Tidy up usings

--- src/SFA.DAS.Recruit.Jobs/DataAccess/Sql/HostBuilderExtensions.cs
+++ src/SFA.DAS.Recruit.Jobs/DataAccess/Sql/HostBuilderExtensions.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SFA.DAS.Recruit.Jobs.Features.ApplicationReviewsMigration;
 
 namespace SFA.DAS.Recruit.Jobs.DataAccess.Sql;
 
