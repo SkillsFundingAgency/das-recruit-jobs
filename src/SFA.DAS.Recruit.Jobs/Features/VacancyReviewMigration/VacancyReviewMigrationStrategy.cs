727f7f254035a7203acb150a188316c8b44b613d
killij <51908793+killij@users.noreply.github.com>
2025-04-17 13:59:13 +0100

    FAI-2483 Add a few more logging statements

--- src/SFA.DAS.Recruit.Jobs/Features/VacancyReviewMigration/VacancyReviewMigrationStrategy.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/VacancyReviewMigration/VacancyReviewMigrationStrategy.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Recruit.Jobs.DataAccess.MongoDb.Domain;
 using SqlVacancyReview = SFA.DAS.Recruit.Jobs.DataAccess.Sql.Domain.VacancyReview;
 using MongoVacancyReview = SFA.DAS.Recruit.Jobs.DataAccess.MongoDb.Domain.VacancyReview;
@@ -7,6 +8,7 @@ namespace SFA.DAS.Recruit.Jobs.Features.VacancyReviewMigration;
 
 [ExcludeFromCodeCoverage]
 public class VacancyReviewMigrationStrategy(
+    ILogger<VacancyReviewMigrationStrategy> logger,
     VacancyReviewMapper mapper,
     VacancyReviewMigrationMongoRepository mongoRepository,
     VacancyReviewMigrationSqlRepository sqlRepository)
@@ -28,6 +30,8 @@ public class VacancyReviewMigrationStrategy(
 
     private async Task ProcessBatchAsync(List<MongoVacancyReview> mongoVacancyReviews)
     {
+        logger.LogInformation("Started processing a batch of {count} records", mongoVacancyReviews.Count);
+        
         var apprenticeships = mongoVacancyReviews.Where(x => x.VacancySnapshot.VacancyType is null or VacancyType.Apprenticeship).ToList();
         var traineeships = mongoVacancyReviews.Except(apprenticeships).ToList();
 
@@ -50,18 +54,23 @@ public class VacancyReviewMigrationStrategy(
             if (excluded is { Count: > 0 })
             {
                 await mongoRepository.UpdateFailedMigrationDateBatchAsync(excluded.Select(x => x.Id).ToList());
+                logger.LogInformation("Failed to migrate {FailedCount} vacancy reviews", excluded.Count);
             }
 
             if (mappedVacancyReviews is { Count: > 0 })
             {
                 await sqlRepository.UpsertVacancyReviewBatchAsync(mappedVacancyReviews);
+                logger.LogInformation("Imported {count} vacancy reviews", mappedVacancyReviews.Count);
+                
                 await mongoRepository.UpdateSuccessMigrationDateBatchAsync(mappedVacancyReviews.Select(x => x.Id).ToList());
+                logger.LogInformation("Marked {SuccessCount} vacancy reviews as migrated", mappedVacancyReviews.Count);
             }
         }
 
         if (traineeships is { Count: > 0 })
         {
             await mongoRepository.UpdateIgnoreMigrationBatchAsync(traineeships.Select(x => x.Id).ToList());
+            logger.LogInformation("Marked {IgnoreCount} vacancy reviews for traineeships as 'ignore'", traineeships.Count);
         }
     }
 }
