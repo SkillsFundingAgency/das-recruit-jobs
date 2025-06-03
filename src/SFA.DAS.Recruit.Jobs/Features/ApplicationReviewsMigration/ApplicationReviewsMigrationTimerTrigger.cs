862010a736267aff09f94c138e9a5b6e1b62b13c
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-06-03 12:32:41 +0100

    FAI-2488 - change schedule to every 15 minutes

--- src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/ApplicationReviewsMigrationTimerTrigger.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/ApplicationReviewsMigrationTimerTrigger.cs
@@ -12,7 +12,7 @@ public class ApplicationReviewsMigrationTimerTrigger(
     private const string TriggerName = nameof(ApplicationReviewsMigrationTimerTrigger);
     
     [Function(TriggerName)]
-    public async Task Run([TimerTrigger("*/5 23-5 * * *")] TimerInfo timerInfo)
+    public async Task Run([TimerTrigger("*/5 23-15 * * *")] TimerInfo timerInfo)
     {
         logger.LogInformation("[{TriggerName}] Trigger fired", TriggerName);
         try
