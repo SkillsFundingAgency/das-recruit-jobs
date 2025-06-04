0e4cfba1a6af3e1cef12d4ebf4998aec9927ec24
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-06-04 14:47:51 +0100

    FAI-2488 - Try injecting the mongo client

--- src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/HostBuilderExtensions.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/HostBuilderExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Authentication;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using SFA.DAS.Recruit.Jobs.DataAccess.MongoDb;
 
 namespace SFA.DAS.Recruit.Jobs.Features.ApplicationReviewsMigration;
 
@@ -16,6 +20,21 @@ public static class HostBuilderExtensions
             services.AddTransient<LegacyApplicationMapper>();
             services.AddTransient<ApplicationReviewsMigrationMongoRepository>();
             services.AddTransient<ApplicationReviewMigrationStrategy>();
+            
+            services.AddSingleton<IMongoClient>(sp => {
+                var connectionString = _.Configuration.GetConnectionString("MongoDb");
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+                settings.SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 };
+                settings.ConnectTimeout = TimeSpan.FromMinutes(10);
+                settings.SocketTimeout = TimeSpan.FromMinutes(10);
+                return new MongoClient(settings);
+            });
+
+            services.AddSingleton(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase(MongoDbNames.RecruitDb);
+            });
         });
     }
 }
