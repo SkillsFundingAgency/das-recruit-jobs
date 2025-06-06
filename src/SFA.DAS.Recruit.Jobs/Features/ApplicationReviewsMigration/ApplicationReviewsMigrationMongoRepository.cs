8c9241aa3f4f91356857b4d464c0984587ada61f
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-06-06 12:52:23 +0100

    FAI-2488 - Correct mapping and set to re-import

--- src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/ApplicationReviewsMigrationMongoRepository.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/ApplicationReviewsMigration/ApplicationReviewsMigrationMongoRepository.cs
@@ -20,7 +20,9 @@ public class ApplicationReviewsMigrationMongoRepository(
         
         return await RetryPolicy.ExecuteAsync(
             _ => collection
-                .Find(x=>x.CreatedDate >= new DateTime(2024,01,01) && x.MigrationDate == null && x.MigrationFailed == null, new FindOptions{MaxTime = TimeSpan.FromMinutes(10), BatchSize = batchSize})
+                .Find(x=>x.CreatedDate >= new DateTime(2024,01,01) 
+                         && x.MigrationDate < new DateTime(2025,06,06) 
+                         && x.MigrationFailed == null, new FindOptions{MaxTime = TimeSpan.FromMinutes(10), BatchSize = batchSize})
                 .Limit(batchSize)
                 .ToListAsync(),
             new Context(nameof(FetchBatchAsync))
