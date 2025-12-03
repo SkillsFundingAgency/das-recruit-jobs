414af80fde0ba9c10542ddafef4a2f1b060eaaf6
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-12-03 23:30:59 +0000

    Removed extra properties in the VacancyAnalytics Entity

--- src/SFA.DAS.Recruit.Jobs/DataAccess/Sql/Domain/VacancyAnalytics.cs
+++ src/SFA.DAS.Recruit.Jobs/DataAccess/Sql/Domain/VacancyAnalytics.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json;
 
 namespace SFA.DAS.Recruit.Jobs.DataAccess.Sql.Domain;
 public class VacancyAnalytics
@@ -11,44 +10,4 @@ public class VacancyAnalytics
     [Column(TypeName = "nvarchar(max)")]
     [MaxLength(4000)]
     public string Analytics { get; set; } = string.Empty;
-
-    private bool _parsed = false;
-    private List<VacancyAnalyticsItem>? _cache;
-
-    private void ParseIfNeeded()
-    {
-        if (_parsed)
-            return;
-
-        _parsed = true;
-
-        try
-        {
-            _cache = JsonSerializer.Deserialize<List<VacancyAnalyticsItem>>(Analytics)
-                     ?? [];
-        }
-        catch
-        {
-            _cache = [];
-        }
-    }
-
-    [NotMapped]
-    public List<VacancyAnalyticsItem> AnalyticsData
-    {
-        get
-        {
-            ParseIfNeeded();
-            return _cache!;
-        }
-    }
-}
-
-public class VacancyAnalyticsItem
-{
-    public DateTime AnalyticsDate { get; set; }
-    public int ViewsCount { get; set; }
-    public int SearchResultsCount { get; set; }
-    public int ApplicationStartedCount { get; set; }
-    public int ApplicationSubmittedCount { get; set; }
 }
