bbe921e2c9ad975f103ed5ec9b19c45940af7568
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-12-16 09:38:45 +0000

    FAI-2924 - Update timer frequency

--- src/SFA.DAS.Recruit.Jobs/Features/VacancyMigration/VacancyMigrationTimerTrigger.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/VacancyMigration/VacancyMigrationTimerTrigger.cs
@@ -10,7 +10,7 @@ public class VacancyMigrationTimerTrigger(ILogger<VacancyMigrationTimerTrigger> 
     private const string TriggerName = nameof(VacancyMigrationTimerTrigger);
     
     [Function(TriggerName)] // disable until the Users have properly migrated
-    public async Task Run([TimerTrigger("*/5 23-3 * * *")] TimerInfo timerInfo)
+    public async Task Run([TimerTrigger("*/10 * * * *")] TimerInfo timerInfo)
     {
         logger.LogInformation("[{TriggerName}] Trigger fired", TriggerName);
         try
