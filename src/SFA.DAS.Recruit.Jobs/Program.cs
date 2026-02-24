53a89c99923acb9133e107e3fdc47fbfa18c156d
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-02-24 22:40:57 +0000

    FAI-3002 - add missing attribute

--- src/SFA.DAS.Recruit.Jobs/Program.cs
+++ src/SFA.DAS.Recruit.Jobs/Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Hosting;
 using SFA.DAS.Recruit.Jobs.Core.Configuration;
 
+[assembly: NServiceBusTriggerFunction("SFA.DAS.Recruit.Jobs")]
 var host = new HostBuilder()
     .ConfigureRecruitJobs()
     .Build();
