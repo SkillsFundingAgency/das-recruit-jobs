7c6b78c5d4110f95687fb65079a4eec5d1d8f2bf
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-02-25 17:48:35 +0000

    FAI-3002 - Add rule name shortner and missing packages

--- src/SFA.DAS.Recruit.Jobs/NServiceBus/ConfigureNServiceBusExtension.cs
+++ src/SFA.DAS.Recruit.Jobs/NServiceBus/ConfigureNServiceBusExtension.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Hosting;
 using System.Net;
+using System.Security.Cryptography;
 
 namespace SFA.DAS.Recruit.Jobs.NServiceBus;
 
@@ -11,7 +12,7 @@ public static class ConfigureNServiceBusExtension
     {
         hostBuilder.UseNServiceBus((config, endpointConfiguration) =>
         {
-            //endpointConfiguration.Transport.SubscriptionRuleNamingConvention = AzureRuleNameShortener.Shorten;
+            endpointConfiguration.Transport.SubscriptionRuleNamingConvention = AzureRuleNameShortener.Shorten;
             
             endpointConfiguration.AdvancedConfiguration.EnableInstallers();
             endpointConfiguration.AdvancedConfiguration.SendFailedMessagesTo(ErrorEndpointName);
@@ -49,4 +50,22 @@ public static class ConfigureNServiceBusExtension
            t.Namespace.StartsWith("Esfa.", StringComparison.CurrentCultureIgnoreCase) &&
            t.Namespace.EndsWith(namespaceSuffix);
     
+}
+
+internal static class AzureRuleNameShortener
+{
+    private const int AzureServiceBusRuleNameMaxLength = 50;
+
+    public static string Shorten(Type type)
+    {
+        var ruleName = type.FullName;
+        if (ruleName!.Length <= AzureServiceBusRuleNameMaxLength)
+        {
+            return ruleName;
+        }
+
+        var bytes = System.Text.Encoding.Default.GetBytes(ruleName);
+        var hash = MD5.HashData(bytes);
+        return new Guid(hash).ToString();
+    }
 }
