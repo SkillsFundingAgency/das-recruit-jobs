e80c562e2e552d2da36eff053ff208d556e84f4f
killij <51908793+killij@users.noreply.github.com>
2026-02-26 13:31:46 +0000

    Revert "FAI-3002 - try and get it to register"
    This reverts commit 9f75f84d73fd25efbe3c60fdb8ca5050246c40a5.

--- src/SFA.DAS.Recruit.Jobs/Features/AiVacancyReviewing/VacancyReviewCreatedEvent.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/AiVacancyReviewing/VacancyReviewCreatedEvent.cs
@@ -1,8 +1,4 @@
 ﻿// ReSharper disable once CheckNamespace -- THIS MUST STAY LIKE THIS TO MATCH THE EVENT FROM RECRUIT INNER
 namespace SFA.DAS.Recruit.Api.Core.Events;
 
-public class VacancyReviewCreatedEvent
-{
-    public Guid VacancyId { get; set; }
-    public Guid VacancyReviewId { get; set; }
-}
+public sealed record VacancyReviewCreatedEvent(Guid VacancyId, Guid VacancyReviewId) : IEvent;
