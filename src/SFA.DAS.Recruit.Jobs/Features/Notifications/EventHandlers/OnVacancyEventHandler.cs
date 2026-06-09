2f121577b2bc2d264696d5c16c0085e04c79b41a
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-06-09 10:15:06 +0100

    Removing debugger

--- src/SFA.DAS.Recruit.Jobs/Features/Notifications/EventHandlers/OnVacancyEventHandler.cs
+++ src/SFA.DAS.Recruit.Jobs/Features/Notifications/EventHandlers/OnVacancyEventHandler.cs
@@ -1,6 +1,5 @@
 ﻿using Esfa.Recruit.Vacancies.Client.Domain.Events;
 using Microsoft.Extensions.Logging;
-using Polly;
 using SFA.DAS.Recruit.Jobs.Core.Infrastructure;
 using SFA.DAS.Recruit.Jobs.Domain;
 using SFA.DAS.Recruit.Jobs.OuterApi.Common;
@@ -17,13 +16,7 @@ public class OnVacancyEventHandler(ILogger<OnVacancyEventHandler> logger,
 {
     private async Task SendNotifications(Guid vacancyId, VacancyStatus? status = null, CancellationToken cancellationToken = default)
     {
-        logger.LogInformation("1. SendNotification for the vacancy: {Id}, Status: {Status}", vacancyId, status);
-        
         var notifications = await notificationService.CreateVacancyNotificationsAsync(vacancyId, status, cancellationToken);
-        foreach (var notificationEmail in notifications)
-        {
-            logger.LogInformation("2. Notifications received for template: {templateId} and Email Address: {email}", notificationEmail.TemplateId, notificationEmail.RecipientAddress); 
-        }
         foreach (var notification in notifications
                      .DistinctBy(x => new // Ensure we only send one notification per template and recipient address
                      {
@@ -32,7 +25,6 @@ public class OnVacancyEventHandler(ILogger<OnVacancyEventHandler> logger,
                      }))
         {
             notification.RecipientAddress = "Balaji.JAMBULINGAM@education.gov.uk";
-            logger.LogInformation("3. Sending email template: {templateId} and Email Address: {email}", notification.TemplateId, notification.RecipientAddress);
             await queueClient.SendMessageAsync(notification, cancellationToken);
         }
     }
@@ -54,18 +46,6 @@ public class OnVacancyEventHandler(ILogger<OnVacancyEventHandler> logger,
     public async Task Handle(VacancyReferredEvent message, IMessageHandlerContext context)
     {
         logger.LogInformation("Event Name: {EventName}, MessageId: {MessageId}", nameof(VacancyReferredEvent), context.MessageId);
-        logger.LogInformation("VacancyId: {VacancyId}, VacancyReference: {VacRef}", message.VacancyId, message.VacancyReference);
-
-        foreach (var header in context.MessageHeaders)
-        {
-            logger.LogInformation("Header {Key}: {Value}", header.Key, header.Value);
-        }
-
-        logger.LogInformation(
-            "MessageId={MessageId}, OriginatingEndpoint={OriginatingEndpoint}, CorrelationId={CorrelationId}",
-            context.MessageId,
-            context.MessageHeaders.GetValueOrDefault("NServiceBus.OriginatingEndpoint"),
-            context.MessageHeaders.GetValueOrDefault("NServiceBus.CorrelationId"));
 
         await SendNotifications(message.VacancyId, cancellationToken: context.CancellationToken);
     }
